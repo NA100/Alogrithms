7ed77376d9845d82b0a4cc1e4f25c7a236552eea
Neha Admal <nehaadmal@Nehas-MacBook-Pro.local>
2025-08-27 17:29:34 -0500

    sorted array median

--- src/algorithms/arrays/sorted_array_median.py
+++ src/algorithms/arrays/sorted_array_median.py
@@ -20,4 +20,53 @@ Brute-force
 
 Optiomal solution
 * Use binary search log(m+n)
+
+
+Here's the intuition that helped me understand this. In this problem, we are searching for the
+"correct partition" in an array, such that,
+
+1. Number of elements in the merged array is (m+n) // 2
+2. All the elements in the left partition of both the arrays are lesser than or equal to all the
+elements in the right partition of both the arrays
+3. If ALeft > BRight --- move the partition to right in nums2 (i.e. move to left in nums1)
+4. If BLeft > ARight --- move the partition to left in nums2 (i.e. move to right in nums1)
+
+
 """
+
+def find_median(nums1, nums2):
+    total = (len(nums1) + len(nums2))
+    half = total // 2
+
+    if nums1 > nums2:
+        nums1, nums2 = nums2, nums1
+
+    l = 0
+    r = len(nums1) - 1
+
+    while True:
+        i = (l + r) // 2  #partition of nums1
+        j = half - i - 2  #partition of nums2
+
+        a_left = nums1[i] if i >= 0 else float('-inf')
+        a_right = nums1[i+1] if (i + 1) < len(nums1) else float('inf')
+        b_left = nums2[j] if j >= 0 else float('-inf')
+        b_right = nums2[j+1] if (j + 1) < len(nums1) else float('inf')
+
+        if (a_left <= b_right) and (b_left <= a_right):
+            if total % 2:
+                return min(a_right, b_right)
+            else:
+                return (max(a_left, b_left) + min(b_right, a_right)) / 2
+        elif a_left > b_right:
+            r = i - 1
+        else:
+            l = i + 1
+
+    return 0
+
+if __name__ == "__main__":
+    nums1 = [1,3]
+    nums2 = [2]
+    median = find_median(nums1, nums2)
+    print(f"Median is {median}")
